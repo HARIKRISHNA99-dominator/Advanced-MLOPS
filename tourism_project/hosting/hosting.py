da39d1fd8fe2954ca6c1bd8ec4a3db0ad103f511
HARIKRISHNA99-dominator <krishnah98.com>
2026-06-14 12:43:03 +0000

    Full project structure: Data, Model, App, and Hosting scripts

---
+++ tourism_project/hosting/hosting.py
@@ -0,0 +1,14 @@
+
+# Pushes all deployment files to Hugging Face Space
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+api.upload_folder(
+    folder_path="tourism_project/deployment",      # local folder with Dockerfile, app.py, requirements.txt
+    repo_id="carnage-colossus/Advanced-MLops",  # your HF Space
+    repo_type="space",
+    path_in_repo="",
+)
+print("Deployment files pushed to Hugging Face Space.")
